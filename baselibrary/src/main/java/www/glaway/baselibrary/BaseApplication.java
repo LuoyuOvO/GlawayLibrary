3b6d3be343698306ca30c70de9111d9e0ebda2f2
LuoyuOvO <2277907666@qq.com>
2020-11-09 13:40:59 +0800

    1

--- baselibrary/src/main/java/www/glaway/baselibrary/BaseApplication.java
+++ baselibrary/src/main/java/www/glaway/baselibrary/BaseApplication.java
@@ -2,6 +2,28 @@ package www.glaway.baselibrary;
 
 import android.app.Application;
 
-public class BaseApplication extends Application {
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 
+import pl.com.salsoft.sqlitestudioremote.SQLiteStudioService;
+
+public abstract class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化SQLiteStudio
+        SQLiteStudioService.instance().start(this);
+        //初始化logger日志
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        //初始化文件压缩/解压
+        ZXingLibrary.initDisplayOpinion(this);
+        //配置数据库
+        initGreenDao();
+
+    }
+
+
+    public abstract  void initGreenDao();
 }
